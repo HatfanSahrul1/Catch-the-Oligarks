d7e7d9a6ae509151cb254de62fe235980f09fa79
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-03-19 18:00:38 +0700

    add : create parallax effect

---
+++ Assets/TemporaryScript/parallax/ParallaxBackground.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    float starPos, length;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    void Start(){
+        starPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate(){
+        float distance = cam.transform.position.x * parallaxEffect;
+        float movement = cam.transform.position.x * (1 - parallaxEffect);
+
+        transform.position = new Vector3(starPos + distance, transform.position.y, transform.position.z);
+
+        if(movement > starPos + length){
+            starPos += length;
+        }
+        else if(movement < starPos - length){
+            starPos -= length;
+        }
+    }
+}
