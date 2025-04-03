364b1939d80cbd21d3d28e4fd2ac7a24485661b4
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-04-04 00:18:32 +0700

    add : create hook class for hook projectile

---
+++ Assets/TemporaryScript/Hook.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Vector2 targetPoint;
+    private float travelTime;
+
+    public void SetTarget(Vector2 target, float time)
+    {
+        targetPoint = target;
+        travelTime = time;
+        StartCoroutine(MoveToTarget());
+    }
+
+    private IEnumerator MoveToTarget()
+    {
+        Vector2 startPos = transform.position;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < travelTime)
+        {
+            transform.position = Vector2.Lerp(startPos, targetPoint, elapsedTime / travelTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPoint;
+        Destroy(gameObject, 0.5f); // Hancurkan proyektil setelah mencapai target
+    }
+}
