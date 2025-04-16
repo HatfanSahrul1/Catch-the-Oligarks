005cd3848a7bb8dfb2b25e91ec59b1daeebc42d7
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-04-17 02:47:55 +0700

    update : make enemy look at player

--- Assets/TemporaryScript/EnemyAI.cs
+++ Assets/TemporaryScript/EnemyAI.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour 
 {
+    GameObject player;
+    void Start(){
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    void Update(){
+        LookAtPlayer();
+    }
+
+    void LookAtPlayer(){
+        Vector3 scale = transform.localScale;
+        if (transform.position.x > player.transform.position.x){
+            scale.x = -Mathf.Abs(scale.x);
+        }
+        else{
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player")){
