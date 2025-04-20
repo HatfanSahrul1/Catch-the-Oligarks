77efdb69573d9d7b92a631895e59c50173bcf8f4
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-04-20 16:08:42 +0700

    add : GrapplePuller class for pull enemy from it's position to destination

---
+++ Assets/TemporaryScript/GrapplePuller.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class GrapplePuller : MonoBehaviour
+{
+    public static GrapplePuller Instance;
+    public float pullSpeed = 100f;
+
+    void Awake(){
+        Instance = this;
+    }
+
+    void Start(){
+        Instance = this;
+    }
+    public void StartPull(Transform target, Transform destination)
+    {
+        StartCoroutine(PullToTarget(target, destination));
+    }
+
+    IEnumerator PullToTarget(Transform target, Transform destination)
+    {
+        while (target != null && Vector3.Distance(target.position, destination.position) > 0.1f)
+        {
+            target.position = Vector3.MoveTowards(target.position, destination.position, pullSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
