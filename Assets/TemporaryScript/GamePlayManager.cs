4454b731c8878bbd3097455cbb341cbd56fc8a18
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-04-24 08:35:06 +0700

    feat : create several function for transition from home to play state

--- Assets/TemporaryScript/GamePlayManager.cs
+++ Assets/TemporaryScript/GamePlayManager.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 public class GamePlayManager : MonoBehaviour 
 {
     public static GamePlayManager Instance { get; private set; }
+    GameStateEnum gameStateEnum;
+
+    [SerializeField] GameObject gate;
 
     void Awake(){
         if(Instance == null) Instance = this;
+    }
+
+    void Update(){
+        gameStateEnum = GameStateManager.Instance.GetCurrentState();
+
+        switch(gameStateEnum){
+            case GameStateEnum.Home: Home(); break;
+        }
+    }
+
+    void Home(){
+        if(Input.anyKeyDown){
+            gate.SetActive(false);
+            CameraFollow.Instance.PressPlay();
+        }
+
+        //switch ke play
+        if(Movement.Instance.IsGround()){
+            GameStateManager.Instance.SwitchState(GameStateEnum.Play);
+            Display.Instance.SetVisibility(true);
+        }
     }
 }
