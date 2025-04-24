50d725a6df68ec1efde7259d4707b4a86c053816
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-04-24 08:36:33 +0700

    feat : adding new Instance and set visibility of UI

--- Assets/TemporaryScript/Display.cs
+++ Assets/TemporaryScript/Display.cs
@@ -4,11 +4,14 @@ using TMPro;
 
 public class Display : MonoBehaviour 
 {
+    public static Display Instance;
     [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] GameObject displayUI;
 
     Info info;
 
     void Awake(){
+        if (Instance == null) Instance = this;
         info = Info.Instance;
         if (info != null){
             info.OnScoreChanged += this.UpdateScore;
@@ -16,6 +19,11 @@ public class Display : MonoBehaviour
     }
 
     void Start(){
+        if(GameStateManager.Instance.GetCurrentState() == GameStateEnum.Play){
+            SetVisibility(true);
+        }else{
+            SetVisibility(false);
+        }
         if (info == null){
             info = Info.Instance;
             info.OnScoreChanged += this.UpdateScore;
@@ -27,5 +35,9 @@ public class Display : MonoBehaviour
 
     public void UpdateScore(int score){
         scoreText.text = score.ToString();
+    }
+
+    public void SetVisibility(bool visible){
+        displayUI.SetActive(visible);
     }
 }
